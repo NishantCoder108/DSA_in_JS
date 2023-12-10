53189f77f21fc8df4d137306ec7bc32b417067ea
Nishant Kumar <nishantcoder108@gmail.com>
2023-12-10 18:39:24 +0530

    group anagram problem

---
+++ Array_Hashing/group_anagrams.js
@@ -0,0 +1,62 @@
+//https://leetcode.com/problems/group-anagrams/submissions/
+
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function (strs) {
+  const hash = {};
+
+  for (let el of strs) {
+    const sortEl = el.split("").sort().join("");
+
+    if (!hash[sortEl]) {
+      hash[sortEl] = [el];
+    } else {
+      hash[sortEl].push(el);
+    }
+  }
+
+  console.log({ hash });
+
+  return Object.values(hash);
+};
+
+/**
+ * Problem Statement
+ * 
+ * Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+Example 2:
+
+Input: strs = [""]
+Output: [[""]]
+Example 3:
+
+Input: strs = ["a"]
+Output: [["a"]]
+ 
+
+Constraints:
+
+1 <= strs.length <= 104
+0 <= strs[i].length <= 100
+strs[i] consists of lowercase English letters.
+ */
+
+el = "rem";
+const sortEl = el.split("");
+const sortElS = sortEl.sort();
+const j = sortElS.join("");
+
+console.log({ sortEl });
+console.log({ sortElS });
+console.log({ j });
