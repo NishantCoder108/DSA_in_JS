3f7f4c8040e305442f9edb3d6c224196ee3b075e
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2023-12-12 15:37:24 +0530

    generate parenthesis

---
+++ Stack/generate_parenthesie.js
@@ -0,0 +1,62 @@
+//https://leetcode.com/problems/generate-parentheses/submissions/
+
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function (n) {
+  const result = [];
+
+  // Helper function to generate combinations recursively
+  function generate(combo, open, close) {
+    if (combo.length === 2 * n) {
+      result.push(combo);
+      return;
+    }
+
+    if (open < n) {
+      generate(combo + "(", open + 1, close);
+    }
+
+    if (close < open) {
+      generate(combo + ")", open, close + 1);
+    }
+  }
+
+  generate("", 0, 0);
+  return result;
+};
+/**
+ * Problem Statement
+ *Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+ 
+
+Example 1:
+
+Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+Example 2:
+
+Input: n = 1
+Output: ["()"]
+ 
+
+Constraints:
+
+1 <= n <= 8
+
+
+
+
+
+ */
+/**
+ *Concepts
+For a given positive integer `n`, the number of valid combinations of well-formed parentheses is given by the nth Catalan number. The formula for the nth Catalan number, denoted as Cn, is:
+
+
+  Formula for the nth Catalan number:
+  C_n = (2n)! / ((n + 1)! * n!)
+
+ */
