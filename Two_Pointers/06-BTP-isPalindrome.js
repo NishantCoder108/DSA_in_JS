8338542ee7fe8bfd54a4f1034cc78e6deeebcbeb
Nishant | Js <69218488+NishantCoder108@users.noreply.github.com>
2023-12-15 19:14:22 +0530

    valid-palindrome by Basic Two Pointer Method Technique 🚀

---
+++ Two_Pointers/06-BTP-isPalindrome.js
@@ -0,0 +1,76 @@
+/**
+ * Basic Two Pointer Method Technique
+ * --------------------------------------
+ *  https://leetcode.com/problems/valid-palindrome/submissions/
+ *  */
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+  let res = false;
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    // Skip non-alphanumeric characters from the left
+    while (left < right && !/^[A-Za-z0-9]+$/.test(s[left])) {
+      left++;
+    }
+
+    // Skip non-alphanumeric characters from the right
+    while (left < right && !/^[A-Za-z0-9]+$/.test(s[right])) {
+      right--;
+    }
+
+    // Compare the lowercase characters at left and right
+    if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+      return false; // Not a palindrome
+    }
+
+    left++;
+    right--;
+  }
+
+  return true;
+};
+// console.log(isPalindrome("A man, a plan, a canal: Panama")); //true
+// console.log(isPalindrome("race a car")); //false
+// console.log(isPalindrome(" ")); //true
+
+console.log(/^[A-Za-z0-9]+$/.test(""));
+
+/**
+ * Problem Statement---
+ * 
+ * 
+ A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+Example 3:
+
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+ 
+
+Constraints:
+
+1 <= s.length <= 2 * 105
+s consists only of printable ASCII characters.
+ */
